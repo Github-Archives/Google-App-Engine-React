3264e61ba30b962c1861a84406e738fda7369a2c
Github-Archives <drewreynerwilliams@gmail.com>
2022-06-24 02:06:26 -0400

    left while trying to get Apollo working might work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,97 @@
-import placeholderImage from './Media/placeholder.jpeg';
-import './App.css';
+// import placeholderImage from './Media/placeholder.jpeg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={placeholderImage} alt="Logo" />
+//         <p>
+//           <h3>
+//             Het is al geruime tijd een bekend gegeven dat een lezer, 
+//             tijdens het bekijken van de layout van een pagina, afgeleid 
+//             wordt door de tekstuele inhoud!!!
+//         </h3>
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from "react"
+import { useQuery } from "@apollo/react-hooks"
+import gql from "graphql-tag"
+import "./App.css"
+
+
+const axios = require("axios");
+
+/**
+ * Using the POST method provided by Axios.
+ * The first parameter accepts the target URL.
+ * The second parameter accepts an object consisting of multiple key-value-pairs.
+ * In this example, we're using the second parameter to send the firstname, middlename, and lastname of the user.
+ */
+ axios
+ .post("https://graphql.anilist.co", {
+     firstname: "Tara",
+     middlename: "Prasad",
+     lastname: "Routray",
+ })
+ .then((response) => {
+     /**
+      * The 'then' method is executed only when the request is successfull.
+      */
+     console.log(response);
+ })
+ .catch((err) => {
+     /**
+      * The 'catch' method is executed only when the request fails to complete.
+      */
+     console.log(err);
+ });
+
+
+
+const GET_USERS = gql`
+  {
+    users {
+      id
+      login
+      avatar_url
+    }
+  }
+`
+
+const User = ({ user: { login, avatar_url } }) => (
+  <div className="Card">
+    <div>
+      <img alt="avatar" className="Card--avatar" src={avatar_url} />
+      <h1 className="Card--name">{login}</h1>
+    </div>
+    <a href={`https://github.com/${login}`} className="Card--link">
+      See profile
+    </a>
+  </div> 
+)
 
 function App() {
+  const { loading, error, data } = useQuery(GET_USERS)
+
+  if (error) return <h1>Something went wrong!</h1>
+  if (loading) return <h1>Loading...</h1>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={placeholderImage} alt="Logo" />
-        <p>
-          <h3>
-            Het is al geruime tijd een bekend gegeven dat een lezer, 
-            tijdens het bekijken van de layout van een pagina, afgeleid 
-            wordt door de tekstuele inhoud!!!
-        </h3>
-        </p>
-      </header>
-    </div>
-  );
+    <main className="App">
+      <h1>Github | Users</h1>
+      {data.users.map(user => (
+        <User key={user.id} user={user} />
+      ))}
+    </main>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// .. Original
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import ApolloClient from 'apollo-boost'
+// import { ApolloProvider } from '@apollo/react-hooks';
+
+// import App from './App';
+// import './index.css';
+// // import * as serviceWorker from './serviceWorker';
+
+// const client = new ApolloClient({
+//   uri: 'https://7sgx4.sse.codesandbox.io'
+// })
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ApolloProvider client={client}>
+//       <App />
+//     </ApolloProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// // serviceWorker.unregister();
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache()
+});
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>,
+  rootElement
 );
-
