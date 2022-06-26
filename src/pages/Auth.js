5f5a0ab85e11856e91c7e27ee8f4ede1b5efc5af
Github-Archives <drewreynerwilliams@gmail.com>
2022-06-26 14:07:45 -0400

    add forms

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,13 +1,29 @@
-// import React, { Component } from "react"
 import React, { useState, useEffect } from "react"
 
+import "./Auth.css"
 // my hook based version
 function AuthPage() {
-  return <h1>The Auth Page</h1>
+  return (
+    <form className="auth-form">
+      <div className="form-control">
+        <label htmlFor="email">E-Mail</label>
+        <input type="email" id="email" />
+      </div>
+      <div className="form-control">
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+      </div>
+      <div className="form-actions">
+        <button type="submit">Submit</button>
+        <button type="button">Switch to Signup</button>
+      </div>
+    </form>
+  )
 }
 
 export default AuthPage
 
+// import React, { Component } from "react"
 // // class based like in the video
 // class AuthPage extends Component {
 //   render() {
